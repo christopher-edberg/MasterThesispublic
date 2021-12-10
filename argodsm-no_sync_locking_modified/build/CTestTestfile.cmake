39219551cad6a77fc3f779f110ff020b93231bad
Christopher Edberg <christopher.edberg@gmail.com>
2021-12-10 07:39:09 +0100

    UpÃdated repo with zip version of modified argo

--- argodsm-no_sync_locking_modified/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/21chr/Documents/Universitet/Exjobb/Argo/MasterThesis/argodsm-no_sync_locking_modified
-# Build directory: /mnt/c/Users/21chr/Documents/Universitet/Exjobb/Argo/MasterThesis/argodsm-no_sync_locking_modified/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- argodsm-no_sync_locking_modified/build/tests/gtest-1.7.0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/21chr/Documents/Universitet/Exjobb/Argo/MasterThesis/argodsm-no_sync_locking_modified/tests/gtest-1.7.0
-# Build directory: /mnt/c/Users/21chr/Documents/Universitet/Exjobb/Argo/MasterThesis/argodsm-no_sync_locking_modified/build/tests/gtest-1.7.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
